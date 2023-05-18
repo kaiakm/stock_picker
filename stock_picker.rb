a611c11026761433c1651da40a77dab9cf8fdffa
kaia <89624646+kaiakm@users.noreply.github.com>
2023-05-18 17:02:49 -0500

    done

---
+++ stock_picker.rb
@@ -0,0 +1,19 @@
+def stock_picker(prices)
+    buy_day = 0
+    sell_day = 0
+    max_profit = 0
+
+    (0...prices.length).each do |i|
+        (i+1...prices.length).each do |j|
+            profit = prices[j] - prices [i]
+            if profit > max_profit
+                max_profit = profit
+                buy_day = i
+                sell_day = j
+            end
+        end
+    end
+    [buy_day, sell_day]
+end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
